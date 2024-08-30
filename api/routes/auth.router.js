4d3eef6427fb6146ab19556547a96e42218ace67
Karlova Tetiana <tanyalyt80@gmail.com>
2024-08-30 23:05:10 +0300

    complete sign up page functionality

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { singup } from "../controlers/auth.controler.js";
+import { singin, singup } from "../controlers/auth.controler.js";
 
 const router = express.Router();
 
 router.post("/singup", singup);
+router.post("/singin", singin);
 
 export default router;
